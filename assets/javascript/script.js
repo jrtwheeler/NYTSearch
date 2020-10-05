7c8d59c3c0e387d24d6a20bf859ac1104ba717e1
jrtwheeler <58869889+jrtwheeler@users.noreply.github.com>
2020-10-04 21:30:43 -0400

    Added input select to search form for number of records

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -45,7 +45,7 @@ function queryNYT() {
     var result = response.response;
     console.log(result);
     //create containers for the returned search results
-    for (var i = 0; i < result.docs.length; i++) {
+    for (var i = 0; i < recordTerm.val(); i++) {
       console.log(result.docs[i].headline.main);
       var ResultDiv = $("<header>").addClass("col-12 card mt-3 mb-3")
       var headlineCSS = $("<h5>")
